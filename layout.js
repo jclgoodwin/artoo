86becfbf1dea901564180204c449031bf9eba6f4
Josh Goodwin <jclg500@york.ac.uk>
2015-02-17 10:25:51 +0000

    dagre.js

--- layout.js
+++ layout.js
@@ -1,5 +1,53 @@
 var layout = [];
 
+layout.doDagreLayout = function (graph) {
+    // Create a new directed graph 
+    var g = new dagre.graphlib.Graph(),
+        i,
+        bbox;
+
+    // Set an object for the graph label
+    g.setGraph({});
+
+    // Default to assigning a new object as a label for each new edge.
+    g.setDefaultEdgeLabel(function() { return {}; });
+
+    // Add nodes to the graph. The first argument is the node id. The second is
+    // metadata about the node. In this case we're going to add labels to each of
+    // our nodes.
+    for (i = 0; i < graph.ids.length; i += 1) {
+        bbox = graph.graph.nodes[i].getBBox();
+        g.setNode(graph.ids[i], { width: bbox.width, height: bbox.height });
+    }
+
+    // make edges
+    for (i = 0; i < graph.connections.length; i += 1) {
+        g.setEdge(graph.connections[i].fromId, graph.connections[i].toId);
+    }
+
+    dagre.layout(g);
+
+    g.nodes().forEach(function (v) {
+        var node = g.node(v);
+        if (typeof(node) !== 'undefined') {
+            window.graph.graph.nodes[v].setAttribute("transform", "translate(" + parseInt(node.x) + ", " + parseInt(node.y) + ")");
+        }
+    });
+
+    for (var c = 0; c < graph.graph.connections.length; c++) {
+        drawLine(graph.graph, graph.graph.connections[c]);
+    }
+
+    // Re-draw diamonds
+    for (var n in graph.graph.nodes) {
+        if (graph.graph.nodes.hasOwnProperty(n)) {
+            graph.graph.placeDiamond(n);
+        }
+    }
+
+    graph.graph.centreGraph();
+}
+
 layout.doArborLayout = function (graph) {
     'use strict';
 
@@ -125,11 +173,15 @@ $(document).ready(function () {
 
     $("#applicationMenu").append("<li class=menuLi id=layOut>Lay out (springy.js)</li>");
     $("#applicationMenu").append("<li class=menuLi id=ArborLayOut>Lay out (arbor.js)</li>");
+    $("#applicationMenu").append("<li class=menuLi id=DagreLayOut>Lay out (dagre.js)</li>");
 
     $("#layOut").click(function () {
         layout.doLayout(graph);
     });
     $("#ArborLayOut").click(function () {
         layout.doArborLayout(graph);
+    });
+    $("#DagreLayOut").click(function () {
+        layout.doDagreLayout(graph);
     });
 });
