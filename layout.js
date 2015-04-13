53bc2a9009b014b7c750b58810379d35c3929645
Josh Goodwin <jclg500@york.ac.uk>
2015-04-13 03:01:32 +0100

    two variations of a more novel use of dagre, and some reorganisation

--- layout.js
+++ layout.js
@@ -1,54 +1,218 @@
-var layout = [];
+var layout = {};
 
-layout.doDagreLayout = function (graph) {
-    // Create a new directed graph 
-    var g = new dagre.graphlib.Graph(),
-        i,
+layout.layoutStrategies = {};
+
+layout.layoutStrategies.dagre = function (graph) {
+    "use strict";
+
+    // create a new directed graph 
+    var g = new dagre.graphlib.Graph();
+
+    // declare useful variables
+    var i,
         bbox;
 
-    // Set an object for the graph label
     g.setGraph({});
+    g.setDefaultEdgeLabel(function () { return {}; });
 
-    // Default to assigning a new object as a label for each new edge.
-    g.setDefaultEdgeLabel(function() { return {}; });
-
-    // Add nodes to the graph. The first argument is the node id. The second is
-    // metadata about the node. In this case we're going to add labels to each of
-    // our nodes.
+    // Add nodes to the graph.
     for (i = 0; i < graph.ids.length; i += 1) {
-        bbox = graph.graph.nodes[i].getBBox();
-        g.setNode(graph.ids[i], { width: bbox.width, height: bbox.height });
+        if (graph.graph.nodes[i]) {
+            bbox = graph.graph.nodes[i].getBBox();
+            g.setNode(graph.ids[i], { width: bbox.width, height: bbox.height });
+        }
     }
 
     // make edges
-    for (i = 0; i < graph.connections.length; i += 1) {
-        g.setEdge(graph.connections[i].fromId, graph.connections[i].toId);
+    for (i = 0; i < graph.graph.connections.length; i += 1) {
+        g.setEdge(graph.graph.connections[i].fromId, graph.graph.connections[i].toId);
     }
 
     dagre.layout(g);
 
     g.nodes().forEach(function (v) {
         var node = g.node(v);
-        if (typeof(node) !== 'undefined') {
+        if (typeof node !== "undefined") {
             window.graph.graph.nodes[v].setAttribute("transform", "translate(" + parseInt(node.x) + ", " + parseInt(node.y) + ")");
         }
     });
 
-    for (var c = 0; c < graph.graph.connections.length; c++) {
-        drawLine(graph.graph, graph.graph.connections[c]);
+    for (i = 0; i < graph.graph.connections.length; i++) {
+        drawLine(graph.graph, graph.graph.connections[i]);
     }
 
     // Re-draw diamonds
-    for (var n in graph.graph.nodes) {
-        if (graph.graph.nodes.hasOwnProperty(n)) {
-            graph.graph.placeDiamond(n);
+    for (i in graph.graph.nodes) {
+        if (graph.graph.nodes.hasOwnProperty(i)) {
+            graph.graph.placeDiamond(i);
+        }
+    }
+
+    graph.graph.centreGraph();
+};
+
+layout.layoutStrategies.dagreNovel1 = function (graph) {
+    "use strict";
+
+    function getParent(id) {
+        for (var i = 0; i < graph.connections.length; i += 1) {
+            if (graph.connections[i].toId === id) {
+                return graph.connections[i].fromId;
+            }
+        }
+        return null; // node has no parent (is the top-level one)
+    }
+
+    function setHorizontalEdge(g, fromId, toId) {
+        var parent = getParent(fromId);
+        if (parent === null) {
+            // create a dummy parent node
+            parent = "-" + fromId;
+            g.setNode(parent);
+            g.setEdge(parent, graph.graph.connections[i].fromId);
+        }
+        g.setEdge(parent, graph.graph.connections[i].toId);
+    }
+
+    // create a new directed graph 
+    var g = new dagre.graphlib.Graph();
+
+    // declare useful variables
+    var i,
+        bbox;
+
+    // Set an object for the graph label
+    g.setGraph({});
+
+    // Default to assigning a new object as a label for each new edge.
+    g.setDefaultEdgeLabel(function () { return {}; });
+
+    // Add nodes to the graph.
+    for (i = 0; i < graph.ids.length; i += 1) {
+        if (graph.graph.nodes[i]) {
+            bbox = graph.graph.nodes[i].getBBox();
+            g.setNode(graph.ids[i], { width: bbox.width, height: bbox.height });
+        } else {
+            console.log(not);
+        }
+    }
+
+    // make edges
+    for (i = 0; i < graph.graph.connections.length; i += 1) {
+        if (graph.graph.connections[i].vertical) {
+            g.setEdge(graph.graph.connections[i].fromId, graph.graph.connections[i].toId);
+        } else { // special case for InContextOf connections
+            setHorizontalEdge(g, graph.graph.connections[i].fromId, graph.graph.connections[i].toId);
+        }
+    }
+
+    dagre.layout(g);
+
+    g.nodes().forEach(function (v) {
+        var node = g.node(v);
+        if (typeof node !== "undefined" && typeof window.graph.graph.nodes[v] !== "undefined") {
+            // if (!isDummyNode(v)) {
+                window.graph.graph.nodes[v].setAttribute("transform", "translate(" + parseInt(node.x) + ", " + parseInt(node.y) + ")");
+            // }
+        } else {
+            console.log(v + " was undefined");
+        }
+    });
+
+    for (i = 0; i < graph.graph.connections.length; i++) {
+        drawLine(graph.graph, graph.graph.connections[i]);
+    }
+
+    // Re-draw diamonds
+    for (i in graph.graph.nodes) {
+        if (graph.graph.nodes.hasOwnProperty(i)) {
+            graph.graph.placeDiamond(i);
+        }
+    }
+
+    graph.graph.centreGraph();
+};
+
+layout.layoutStrategies.dagreNovel2 = function (graph) {
+    "use strict";
+
+    function getParent(id) {
+        for (var i = 0; i < graph.connections.length; i += 1) {
+            if (graph.connections[i].toId === id) {
+                return graph.connections[i].fromId;
+            }
+        }
+        return null; // node has no parent (is the top-level one)
+    }
+
+    function setHorizontalEdge(g, fromId, toId) {
+        // create a dummy parent node
+        parent = "-" + fromId;
+        g.setNode(parent);
+        g.setEdge(parent, graph.graph.connections[i].fromId);
+        g.setEdge(parent, graph.graph.connections[i].toId);
+    }
+
+    // create a new directed graph 
+    var g = new dagre.graphlib.Graph();
+
+    // declare useful variables
+    var i,
+        bbox;
+
+    // Set an object for the graph label
+    g.setGraph({});
+
+    // Default to assigning a new object as a label for each new edge.
+    g.setDefaultEdgeLabel(function () { return {}; });
+
+    // Add nodes to the graph.
+    for (i = 0; i < graph.ids.length; i += 1) {
+        if (graph.graph.nodes[i]) {
+            bbox = graph.graph.nodes[i].getBBox();
+            g.setNode(graph.ids[i], { width: bbox.width, height: bbox.height });
+        } else {
+            console.log(not);
+        }
+    }
+
+    // make edges
+    for (i = 0; i < graph.graph.connections.length; i += 1) {
+        if (graph.graph.connections[i].vertical) {
+            g.setEdge(graph.graph.connections[i].fromId, graph.graph.connections[i].toId);
+        } else { // special case for InContextOf connections
+            setHorizontalEdge(g, graph.graph.connections[i].fromId, graph.graph.connections[i].toId);
+        }
+    }
+
+    dagre.layout(g);
+
+    g.nodes().forEach(function (v) {
+        var node = g.node(v);
+        if (typeof node !== "undefined" && typeof window.graph.graph.nodes[v] !== "undefined") {
+            // if (!isDummyNode(v)) {
+                window.graph.graph.nodes[v].setAttribute("transform", "translate(" + parseInt(node.x) + ", " + parseInt(node.y) + ")");
+            // }
+        } else {
+            console.log(v + " was undefined");
+        }
+    });
+
+    for (i = 0; i < graph.graph.connections.length; i++) {
+        drawLine(graph.graph, graph.graph.connections[i]);
+    }
+
+    // Re-draw diamonds
+    for (i in graph.graph.nodes) {
+        if (graph.graph.nodes.hasOwnProperty(i)) {
+            graph.graph.placeDiamond(i);
         }
     }
 
     graph.graph.centreGraph();
 }
 
-layout.doArborLayout = function (graph) {
+layout.layoutStrategies.arbor = function (graph) {
     'use strict';
 
     var system = arbor.ParticleSystem(),
@@ -95,7 +259,7 @@ layout.doArborLayout = function (graph) {
 
 }
 
-layout.doLayout = function (graph) {
+layout.layoutStrategies.springy = function (graph) {
     'use strict';
 
     var springyGraph = new Springy.Graph(),
@@ -150,7 +314,6 @@ layout.doLayout = function (graph) {
 $(document).ready(function () {
 
     // useful "load example" menu
-
     $("#applicationMenu").append("<li class=menuLi id=loadExampleMenuMain>Load example...</li>");
     $("#loadExampleMenuMain").append("<ul class=ulMenu id=loadExampleMenu></ul>");
     for (var i = 1; i <= 4; i += 1) {
@@ -169,19 +332,17 @@ $(document).ready(function () {
         });
     });
 
-    // automatic layout button
+    // automatic layout menu
 
-    $("#applicationMenu").append("<li class=menuLi id=layOut>Lay out (springy.js)</li>");
-    $("#applicationMenu").append("<li class=menuLi id=ArborLayOut>Lay out (arbor.js)</li>");
-    $("#applicationMenu").append("<li class=menuLi id=DagreLayOut>Lay out (dagre.js)</li>");
+    $("#applicationMenu").append("<li class=menuLi id=layOutMenuMain>Lay out...</li>");
+    $("#layOutMenuMain").append("<ul class=ulMenu id=layOutMenu></ul>");
+    $("#layOutMenu").append("<li class=menuLi>springy</li>");
+    $("#layOutMenu").append("<li class=menuLi>dagre</li>");
+    $("#layOutMenu").append("<li class=menuLi>dagreNovel1</li>");
+    $("#layOutMenu").append("<li class=menuLi>dagreNovel2</li>");
 
-    $("#layOut").click(function () {
-        layout.doLayout(graph);
+    $("#layOutMenu li").click(function () {
+        layout.layoutStrategies[this.textContent].call(this, graph);
     });
-    $("#ArborLayOut").click(function () {
-        layout.doArborLayout(graph);
-    });
-    $("#DagreLayOut").click(function () {
-        layout.doDagreLayout(graph);
-    });
+
 });
