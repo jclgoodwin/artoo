86643a763da03170c48d95ddf76b28cfe62af902
Josh Goodwin <jclg500@york.ac.uk>
2015-04-22 23:32:58 +0100

    various refactoring and reliability improvements

--- layout.js
+++ layout.js
@@ -2,10 +2,15 @@ var layout = {};
 
 layout.layoutStrategies = {};
 
+layout.setCoords = function (element, x, y) {
+    var bbox = element.getBBox();
+    element.setAttribute("transform", "translate(" + parseInt(bbox.x + x) + ", " + parseInt(bbox.y + y) + ")");
+}
+
 layout.layoutStrategies.dagre = function (graph) {
     "use strict";
 
-    // create a new directed graph 
+    // create a new directed graph
     var g = new dagre.graphlib.Graph();
 
     // declare useful variables
@@ -33,7 +38,7 @@ layout.layoutStrategies.dagre = function (graph) {
     g.nodes().forEach(function (v) {
         var node = g.node(v);
         if (typeof node !== "undefined") {
-            window.graph.graph.nodes[v].setAttribute("transform", "translate(" + parseInt(node.x) + ", " + parseInt(node.y) + ")");
+            layout.setCoords(window.graph.graph.nodes[v], node.x, node.y);
         }
     });
 
@@ -51,106 +56,15 @@ layout.layoutStrategies.dagre = function (graph) {
     graph.graph.centreGraph();
 };
 
-layout.layoutStrategies.dagreNovel1 = function (graph) {
+layout.layoutStrategies.dagreNovel = function (graph) {
     "use strict";
-
-    function getParent(id) {
-        for (var i = 0; i < graph.connections.length; i += 1) {
-            if (graph.connections[i].toId === id) {
-                return graph.connections[i].fromId;
-            }
-        }
-        return null; // node has no parent (is the top-level one)
-    }
-
-    function setHorizontalEdge(g, fromId, toId) {
-        var parent = getParent(fromId);
-        if (parent === null) {
-            // create a dummy parent node
-            parent = "-" + fromId;
-            g.setNode(parent);
-            g.setEdge(parent, graph.graph.connections[i].fromId);
-        }
-        g.setEdge(parent, graph.graph.connections[i].toId);
-    }
-
-    // create a new directed graph 
-    var g = new dagre.graphlib.Graph();
-
-    // declare useful variables
-    var i,
-        bbox;
-
-    // Set an object for the graph label
-    g.setGraph({});
-
-    // Default to assigning a new object as a label for each new edge.
-    g.setDefaultEdgeLabel(function () { return {}; });
-
-    // Add nodes to the graph.
-    for (i = 0; i < graph.ids.length; i += 1) {
-        if (graph.graph.nodes[i]) {
-            bbox = graph.graph.nodes[i].getBBox();
-            g.setNode(graph.ids[i], { width: bbox.width, height: bbox.height });
-        } else {
-            console.log(not);
-        }
-    }
-
-    // make edges
-    for (i = 0; i < graph.graph.connections.length; i += 1) {
-        if (graph.graph.connections[i].vertical) {
-            g.setEdge(graph.graph.connections[i].fromId, graph.graph.connections[i].toId);
-        } else { // special case for InContextOf connections
-            setHorizontalEdge(g, graph.graph.connections[i].fromId, graph.graph.connections[i].toId);
-        }
-    }
-
-    dagre.layout(g);
-
-    g.nodes().forEach(function (v) {
-        var node = g.node(v);
-        if (typeof node !== "undefined" && typeof window.graph.graph.nodes[v] !== "undefined") {
-            // if (!isDummyNode(v)) {
-                window.graph.graph.nodes[v].setAttribute("transform", "translate(" + parseInt(node.x) + ", " + parseInt(node.y) + ")");
-            // }
-        } else {
-            console.log(v + " was undefined");
-        }
-    });
-
-    for (i = 0; i < graph.graph.connections.length; i++) {
-        drawLine(graph.graph, graph.graph.connections[i]);
-    }
-
-    // Re-draw diamonds
-    for (i in graph.graph.nodes) {
-        if (graph.graph.nodes.hasOwnProperty(i)) {
-            graph.graph.placeDiamond(i);
-        }
-    }
-
-    graph.graph.centreGraph();
-};
-
-layout.layoutStrategies.dagreNovel2 = function (graph) {
-    "use strict";
-
-    function getParent(id) {
-        for (var i = 0; i < graph.connections.length; i += 1) {
-            if (graph.connections[i].toId === id) {
-                return graph.connections[i].fromId;
-            }
-        }
-        return null; // node has no parent (is the top-level one)
-    }
 
     function setHorizontalEdge(g, fromId, toId) {
         // create a dummy parent node
-        parent = "-" + fromId;
+        parent = "-" + toId;
         g.setNode(parent);
-        g.setEdge(parent, graph.graph.connections[i].fromId);
-        g.setEdge(parent, graph.graph.connections[i].toId);
+        g.setEdge(parent, graph.graph.connections[i].fromId, { weight: 2 });
+        g.setEdge(parent, graph.graph.connections[i].toId, { weight: 2 });
     }
 
     // create a new directed graph 
@@ -190,11 +104,7 @@ layout.layoutStrategies.dagreNovel2 = function (graph) {
     g.nodes().forEach(function (v) {
         var node = g.node(v);
         if (typeof node !== "undefined" && typeof window.graph.graph.nodes[v] !== "undefined") {
-            // if (!isDummyNode(v)) {
-                window.graph.graph.nodes[v].setAttribute("transform", "translate(" + parseInt(node.x) + ", " + parseInt(node.y) + ")");
-            // }
-        } else {
-            console.log(v + " was undefined");
+            layout.setCoords(window.graph.graph.nodes[v], node.x, node.y);
         }
     });
 
@@ -229,7 +139,7 @@ layout.layoutStrategies.arbor = function (graph) {
                 if (graph.graph.nodes[node.name] === undefined) {
                     return false;
                 }
-                graph.graph.nodes[node.name].setAttribute("transform", "translate(" + parseInt(p.x * 70) + ", " + parseInt(p.y * 45) + ")");
+                layout.setCoords(graph.graph.nodes[node.name], p.x * 70, parseInt(p.y * 45));
 
                 for (var c = 0; c < graph.graph.connections.length; c++) {
                     drawLine(graph.graph, graph.graph.connections[c]);
@@ -304,11 +214,12 @@ layout.layoutStrategies.springy = function (graph) {
             if (graph.graph.nodes[node.data.svgId] === undefined) {
                 return false;
             }
-            graph.graph.nodes[node.data.svgId].setAttribute("transform", "translate(" + parseInt(p.x * 70) + ", " + parseInt(p.y * 45) + ")");
+            layout.setCoords(graph.graph.nodes[node.data.svgId], p.x * 70, p.y * 45);
         }
     );
 
     springyRenderer.start();
+    springyRenderer.stop();
 }
 
 $(document).ready(function () {
@@ -338,8 +249,7 @@ $(document).ready(function () {
     $("#layOutMenuMain").append("<ul class=ulMenu id=layOutMenu></ul>");
     $("#layOutMenu").append("<li class=menuLi>springy</li>");
     $("#layOutMenu").append("<li class=menuLi>dagre</li>");
-    $("#layOutMenu").append("<li class=menuLi>dagreNovel1</li>");
-    $("#layOutMenu").append("<li class=menuLi>dagreNovel2</li>");
+    $("#layOutMenu").append("<li class=menuLi>dagreNovel</li>");
 
     $("#layOutMenu li").click(function () {
         layout.layoutStrategies[this.textContent].call(this, graph);
